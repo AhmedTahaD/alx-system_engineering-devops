aedfb279fd3c72e26008ecaf09036e23f7c5af28
AhmedTahaD <Ahmedtaha2893@gmail.com>
2024-08-13 23:07:45 +0300

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API and prints the titles
-of the first 10 hot posts listed for a given subreddit."""
+"""This script will return the number of subscribers associated with
+a subreddit
+"""
+import json
+import requests
+from sys import argv
 
 
 def top_ten(subreddit):
-    """function that queries the Reddit API and prints the titles
-    of the first 10 hot posts listed for a given subreddit."""
-    import requests
-    import sys
-
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    try:
-        response = requests.get(url, headers=headers,allow_redirects=False)
-        if response.status_code == 200:
-            children = response.json().get('data').get('children')
-            for i in range(10):
-                print(children[i].get('data').get('title'))
-        else:
-            print("None")
-    except Exception:
-        print("None")
+        """Method get the number of users subscribed to a subreddit
+        subreddit (Str)- subreddit to check
+        Returns - number of users (INT) else 0 (INT) if not subreddit is found
+        """
+        try:
+            header = {'user-agent': 'Mozilla/5.0', 'allow_redirects': 'false'}
+            param = {'limit': 10}
+            url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+            req = requests.get(url, headers=header, params=param).json().get('data')
+            for post in req.get('children'):
+                print(post.get('data', None).get('title', None))
+        except Exception as e:
+            print(None)
+if __name__ == "__main__":
+    pass
